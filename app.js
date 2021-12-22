1b1fb2ecad6f095aaef71ed3daa73b73e5baabbe
Vaibhav kale <vaibhavkale2201@gmaile.com>
2021-12-22 18:00:48 +0530

    changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose=require("mongoose");
 const ejs = require("ejs");
 
 //database code
-mongoose.connect("mongodb+srv://vaihbav2001:<Paaword>@cluster0.ih6g9.mongodb.net/jounralDB");
+mongoose.connect("mongodb+srv://vaihbav2001:Vk%4022012021@cluster0.ih6g9.mongodb.net/jounralDB");
 const itemsSchema = new mongoose.Schema({
   title:String,
   post:String
